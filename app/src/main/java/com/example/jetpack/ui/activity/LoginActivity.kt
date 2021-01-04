f53c11be9400d59d976b071e6aba7f3d22ea197c
肖奇 <xiaoqikillua@163.com>
2021-01-04 10:09:21 +0800

    jetpack 练习 demo  创建

---
+++ app/src/main/java/com/example/jetpack/ui/activity/LoginActivity.kt
@@ -0,0 +1,26 @@
+package com.example.jetpack.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.jetpack.R
+
+class LoginActivity : AppCompatActivity() {
+
+    //lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        /*val host:NavHostFragment = supportFragmentManager.findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment
+        navController = host.navController*/
+
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+
+
+        //navController.navigateUp()
+    }
+}
